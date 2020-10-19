0c1fe5c9ae88fbbcfc809bcb7e82718a73093dbe
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-20 00:00:51 +0300

    5.1: blogilistan frontend, step1
    Toteuta frontendiin kirjautumisen mahdollistava toiminnallisuus.
    	- Kirjautumisen yhteydessä backendin palauttama token tallennetaan sovelluksen tilaan user.
    	- Jos käyttäjä ei ole kirjautunut, sivulla näytetään pelkästään kirjautumislomake.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login
+        ({ username, password, })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log(user)
+    } catch (exception) {
+      console.log('wrong credentials!')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Käyttäjätunnus
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Salasana
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">kirjaudu</button>
+        </form>
+      </div>
+    )
+  }
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
