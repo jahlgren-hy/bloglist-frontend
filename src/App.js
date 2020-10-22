4e6a09ff3f6445dc98cf1e81bee39b84efd29a45
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-22 08:43:25 +0300

    5.9*: blogilistan frontend, step9
    Järjestä sovellus näyttämään blogit likejen mukaisessa suuruusjärjestyksessä.
    
    	- modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -86,9 +86,11 @@ const App = () => {
       <Togglable buttonLabel="New blog">
         <BlogForm onSubmit={addBlog} />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(b =>
+          <Blog key={b.id} blog={b} />
+        )}
     </div>
   )
 
