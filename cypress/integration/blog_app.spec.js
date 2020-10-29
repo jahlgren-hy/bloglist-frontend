6c10456dd06c6bf786c9edf6e9f809de961cc71d
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-29 03:54:27 +0200

    5.20: blogilistan end to end -testit, step4
    Tee testi, joka varmistaa, että blogia voi likettää.
    
    	- modified:   .gitignore
    	- modified:   cypress/integration/blog_app.spec.js
    	- modified:   cypress/support/commands.js
    	- modified:   package.json
    	- modified:   src/App.css

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -31,16 +31,20 @@ describe('Blog app', function () {
       cy.get('#username').type('jukka')
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
-      cy.contains('wrong credentials!')
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials!')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+      //  .and('have.css', 'border-style', 'solid')
+      cy.get('html')
+        .should('not.contain', 'Jukka Ahlgren logged in')
     })
   })
 
 
-  describe.only('When logged in', function () {
+  describe('When logged in', function () {
     beforeEach(function () {
-      cy.get('#username').type('jukka')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.login({ username: 'jukka', password: 'salainen' })
     })
 
     it('A blog can be created', function () {
@@ -51,6 +55,38 @@ describe('Blog app', function () {
       cy.contains('Luo').click()
       cy.contains('a blog created by cypress')
     })
-  })
 
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'first blog',
+          author: 'cypress',
+          url: 'https://localhost'
+        })
+        cy.createBlog({
+          title: 'second blog',
+          author: 'jukka',
+          url: 'https://localhost'
+        })
+        cy.createBlog({
+          title: 'third blog',
+          title: 'a blog created by jukka',
+          author: 'Geist',
+          url: 'https://localhost'
+        })
+      })
+
+      it('it can be liked', function () {
+        cy.contains('first blog')
+          .contains('view')
+          .click()
+
+        cy.contains('like').click()
+        cy.contains('hide').click()
+        cy.contains('view').click()
+        cy.contains('1 likes')
+      })
+    })
+  })
 })
